8952ffbdc4969fd5fe98f4d3d9cf3633ca64ed1f
Ivanpaniagua7 <ivanpan7@hotmail.com>
2021-09-28 22:54:56 -0700

    Lectura de precios Yahoo Finance

--- data.py
+++ data.py
@@ -8,5 +8,22 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+import pandas_datareader.data as web
 
 dict_test = {'key_a': 'a', 'key_b': 'b'}
+
+#%% Función para descargar precios de cierre ajustados:
+def get_adj_closes(tickers, start_date=None, end_date=None):
+    # Fecha inicio por defecto (start_date='2010-01-01') y fecha fin por defecto (end_date=today)
+    # Descargamos DataFrame con todos los datos
+    closes = web.DataReader(name=tickers, data_source='yahoo', start=start_date, end=end_date)
+    # Solo necesitamos los precios ajustados en el cierre
+    closes = closes['Adj Close']
+    # Se ordenan los índices de manera ascendente
+    closes.sort_index(inplace=True)
+    return closes
+
+#%% 
+Closes=get_adj_closes(tickers=['BTC-USD'], start_date=("2020,09,01"))
+
+
